990d8a1d3a17b3edbdb491050fa02a4cb279fadb
Bruno Volpe <bruno.volpe2015@gmail.com>
2025-08-20 23:22:22 -0300

    feat enhance login component with form and layout updates

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import {LoginLayoutComponent} from '../../components/login-layout/login-layout.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginLayoutComponent],
+  imports: [LoginLayoutComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor() {
+
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
 
 }
